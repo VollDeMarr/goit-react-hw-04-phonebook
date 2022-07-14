26b7e6dc586bced2759800e9fe91c970c19275a8
Volodymyr Bobanych <97664943+VollDeMarr@users.noreply.github.com>
2022-07-15 01:08:19 +0300

    Update Phoneboock.js

--- src/components/Phoneboock/Phoneboock.js
+++ src/components/Phoneboock/Phoneboock.js
@@ -1,82 +1,158 @@
-import { Component } from 'react';
+import { useState } from 'react';
+// import { Component } from 'react';
 
 import Contacts from '../Contacts/Contacts';
 import Form from '../Form/Form';
 import Filter from '../Filter/Filter';
 
 import s from './Phoneboock.module.css';
+function Phoneboock() {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
 
-class Phoneboock extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-
-  formSubmit = data => {
-    const { contacts } = this.state;
-
+  const formSubmit = data => {
+    console.log(data);
+    console.log(data.name);
     const findeName = contacts.find(item => item.name === data.name);
     if (findeName) {
       alert(`${data.name} is already in contacts list`);
       return;
     }
-    this.setState(prevState => {
-      return { contacts: [...prevState.contacts, data] };
+    setContacts(prev => {
+      console.log(prev);
+      return [...prev, data];
     });
   };
 
-  removeContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+  const removeContact = id => {
+    console.log(id);
+    setContacts(contact => {
+      contact.filter(cont=> cont.id !== id)
+      console.log(contact)
+    });
+
+    // setContacts(prevState => ({
+    //   contacts: prevState.contacts.filter(contact => contact.id !== id),
+    // }));
   };
 
-  filterList = e => {
-    this.setState({ filter: e.target.value });
+  const filterList = e => {
+    setFilter(e.target.value);
   };
 
-  getVisibleContacts = () => {
-    const { contacts, filter } = this.state;
-
+  const getVisibleContacts = () => {
     const inLowerCase = filter.toLowerCase();
 
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(inLowerCase)
     );
   };
-  componentDidMount() {
-    const contact = localStorage.getItem('contacts');
-    const parsedContact = JSON.parse(contact);
-    if (parsedContact) {
-      this.setState({ contacts: parsedContact });
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-  render() {
-    const filteredContacts = this.getVisibleContacts();
 
-    return (
-      <div className={s.container}>
-        <h1>Phoneboock</h1>
-        <div className={s.inputContainer}>
-          <Form onSubmit={this.formSubmit} />
-        </div>
-        <h2>Contacts</h2>
-        <Filter value={this.state.filter} onChange={this.filterList} />
-        <div className={s.contactsWrapper}>
-          <Contacts contacts={filteredContacts} removeFn={this.removeContact} />
-        </div>
+  // componentDidMount() {
+  //   const contact = localStorage.getItem('contacts');
+  //   const parsedContact = JSON.parse(contact);
+  //   if (parsedContact) {
+  //     this.setState({ contacts: parsedContact });
+  //   }
+  // }
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (this.state.contacts !== prevState.contacts) {
+  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  //   }
+  // }
+
+  const filteredContacts = getVisibleContacts();
+
+  return (
+    <div className={s.container}>
+      <h1>Phoneboock</h1>
+      <div className={s.inputContainer}>
+        <Form onSubmit={formSubmit} />
       </div>
-    );
-  }
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={filterList} />
+      <div className={s.contactsWrapper}>
+        <Contacts contacts={filteredContacts} removeFn={removeContact} />
+      </div>
+    </div>
+  );
 }
+// class Phoneboock extends Component {
+//   state = {
+// contacts: [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+// ],
+//     filter: '',
+//   };
+
+// formSubmit = data => {
+//   const { contacts } = this.state;
+
+//   const findeName = contacts.find(item => item.name === data.name);
+//   if (findeName) {
+//     alert(`${data.name} is already in contacts list`);
+//     return;
+//   }
+//   this.setState(prevState => {
+//     return { contacts: [...prevState.contacts, data] };
+//   });
+// };
+
+// removeContact = id => {
+//   this.setState(prevState => ({
+//     contacts: prevState.contacts.filter(contact => contact.id !== id),
+//   }));
+// };
+
+// filterList = e => {
+//   this.setState({ filter: e.target.value });
+// };
+
+// getVisibleContacts = () => {
+//   const { contacts, filter } = this.state;
+
+//   const inLowerCase = filter.toLowerCase();
+
+//   return contacts.filter(contact =>
+//     contact.name.toLowerCase().includes(inLowerCase)
+//   );
+// };
+// componentDidMount() {
+//   const contact = localStorage.getItem('contacts');
+//   const parsedContact = JSON.parse(contact);
+//   if (parsedContact) {
+//     this.setState({ contacts: parsedContact });
+//   }
+// }
+// componentDidUpdate(prevProps, prevState) {
+//   if (this.state.contacts !== prevState.contacts) {
+//     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//   }
+// }
+// render() {
+//   const filteredContacts = this.getVisibleContacts();
+
+//   return (
+//     <div className={s.container}>
+//       <h1>Phoneboock</h1>
+//       <div className={s.inputContainer}>
+//         <Form onSubmit={this.formSubmit} />
+//       </div>
+//       <h2>Contacts</h2>
+//       <Filter value={this.state.filter} onChange={this.filterList} />
+//       <div className={s.contactsWrapper}>
+//         <Contacts contacts={filteredContacts} removeFn={this.removeContact} />
+//       </div>
+//     </div>
+//   );
+// }
+// }
 export default Phoneboock;
